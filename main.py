fb466f595c31821233269d3d9ee8d24dc3c84e42
Veziik <menendez.daniel2@gmail.com>
2019-03-19 12:54:05 -0400

    synchronizing heads

--- main.py
+++ main.py
@@ -17,11 +17,12 @@ usage: ''' + sys.argv[0] +''' <URL>
 
 Options
 	-v: 					verbose output, prints more to the screen while it works
-	-f <filename>: 			function to use, the hypercrawler will import the given file and use the function called pipe(URL,Response)
+	-f <filename>: 			function to use, requires a class named webpagehandler and the function pipe(url,response)
 	-d <domain 1> ... : 	adds listed domains to the set of authorized domains
 	-t <taboo 1> ... : 		adds listed taboos to the set of keywords websites may not have in order to be visited
-	--Debug: 				debug mode, prints the status of all of the data structures in use for the duration of the run in each iteration
-	--Threads [int]: 		manually specify the number of threads to use'''
+	--debug: 				debug mode, prints the status of all of the data structures in use for the duration of the run in each iteration
+	--threads [int]: 		manually specify the number of threads to use
+	'''
 
 def checkFormat(url):
 	if ('http://' not in url[0:7]) and ('https://' not in url[0:8]):
@@ -31,10 +32,10 @@ def checkFormat(url):
 
 def handleImport(fileName):
 	i = importlib.import_module(fileName.replace('.py',''))
-	return i.pipe
+	return (i.WebpageHandler.pipe,i.WebpageHandler())
 
 def printPageNames(pageUrl, response):
-	print(pageUrl)
+	print(str(len(Pagerunner.visited)) + ': ' + pageUrl)
 
 def parseToSet(startingIndex, numberOfArguments):
 	i = startingIndex + 1
@@ -62,7 +63,8 @@ def parse():
 	arguments['function'] = printPageNames
 	arguments['taboos'] = set()
 	arguments['domains'] = set()
-	
+	arguments['handler'] = None
+
 	if len(sys.argv )>2 :	
 		for i in range(len(sys.argv)):
 			if sys.argv[i].lower() == '--debug':
@@ -72,7 +74,7 @@ def parse():
 			elif sys.argv[i].lower() == '-v':
 				arguments['verbose'] = True
 			elif sys.argv[i].lower() == '-f':
-				arguments['function'] = handleImport(sys.argv[i+1])
+				(arguments['function'],arguments['handler'])= handleImport(sys.argv[i+1])
 			elif sys.argv[i].lower() == '-t': 
 				arguments['taboos'] = parseToSet(i, len(sys.argv))
 			elif sys.argv[i].lower() == '-d': 
